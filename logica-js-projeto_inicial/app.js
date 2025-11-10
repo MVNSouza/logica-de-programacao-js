3fbe2e3a790be7f35c69ad704188ab097723f029
MVNSouza <marcos.nasouza2@gmail.com>
2025-11-10 19:36:47 -0300

    Resolução do desafio inicial

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,24 @@
+
 alert('Boas vindas ao jogo do número secreto!');
 
-let numeroSecreto = prompt("Qual seria o número?");
+let numeroSecreto = 29;
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número de 1 a 30');
+    if (chute == numeroSecreto) {
+        
+        break
+    } else if (chute < numeroSecreto) {
+        alert('O número é maior que o chute!')
+    } else if (chute > numeroSecreto) {
+        alert('O número é menor que o chute!')
+    }
+
+    tentativas++
+}
+
+tentativas > 1 ? alert(`Parabéns! Você acertou o número em apenas ${tentativas} tentativas!`) : alert("Parabéns! Você acertou de primeira!")
+
+
